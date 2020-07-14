147e195b38caa09d0c59f038d4ddc0f214d6b97d
Nick Ray <nickrray@gmail.com>
2020-07-14 17:22:36 -0500

    :bulb: Remove comments for structuring render template string

--- client/components/Input/index.js
+++ client/components/Input/index.js
@@ -4,9 +4,3 @@ function render(input) {
 }
 
 export default (props) => render(props)
-
-// <label for="random">(Random Student)</label>
-// <input type="text" id="random" placeholder="(Random Student)" readonly></input>
-
-// <label for="name" class="screen-reader-text">Name</label>
-// <input id="name" type="text" placeholder="Name"></input>
